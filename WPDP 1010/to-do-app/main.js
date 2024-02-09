f113348d7fd7d1c4aeac14794ecb5067683a351d
Nicole Linnarz <mikaelafs17@gmail.com>
2024-02-09 10:28:19 -0700

    started styling of to-do-app

---
+++ WPDP 1010/to-do-app/main.js
@@ -0,0 +1,10 @@
+// User must be able to create multiple lists of tasks.
+// User must be able to edit, delete, and mark tasks.
+// User must be able to clear tasks when they are complete.
+// User must be able to view all tasks in a list.
+// User must be able to search for specific tasks in the list.
+// User must be able to save tasks for later viewing.
+// User must be able to mark tasks as completed.
+// User must be able to delete tasks from list.
+// User must be able to customize task list view.
+// App must be intuitive and easy to use.
