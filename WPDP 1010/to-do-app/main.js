41adbb407a1baaf1af4d40d8a5eb7e0117017259
Nicole Linnarz <mikaelafs17@gmail.com>
2024-02-13 10:35:16 -0700

    started lists

--- WPDP 1010/to-do-app/main.js
+++ WPDP 1010/to-do-app/main.js
@@ -8,3 +8,48 @@
 // User must be able to delete tasks from list.
 // User must be able to customize task list view.
 // App must be intuitive and easy to use.
+
+let listInput = document.querySelector("#listsInput");
+let addListBtn = document.querySelector("#add-list-btn");
+let listGroup1 = document.querySelector("#list-group1");
+
+//TODO: make sure to add an ID generator(?)
+
+let lists = {
+  1: {
+    id: Math.floor(Math.random() * 100),
+    name: "School",
+    todos: [
+      {
+        text: "To-do app",
+        completed: false,
+      },
+      {
+        text: "Golf Score App",
+        completed: false,
+      },
+      {
+        text: "Team JS App",
+        completed: false,
+      },
+    ],
+  },
+  2: {
+    id: Math.floor(Math.random() * 100),
+    name: "Home",
+    todos: [
+      {
+        text: "Dishes",
+        completed: false,
+      },
+      {
+        text: "Mop",
+        completed: false,
+      },
+      {
+        text: "Laundry",
+        completed: false,
+      },
+    ],
+  },
+};
