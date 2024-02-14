8bfb5ef361febcad8463c4de25b6fb11282660ab
Nicole Linnarz <mikaelafs17@gmail.com>
2024-02-14 10:11:05 -0700

    user can add lists

--- WPDP 1010/to-do-app/main.js
+++ WPDP 1010/to-do-app/main.js
@@ -1,23 +1,6 @@
-// User must be able to create multiple lists of tasks.
-// User must be able to edit, delete, and mark tasks.
-// User must be able to clear tasks when they are complete.
-// User must be able to view all tasks in a list.
-// User must be able to search for specific tasks in the list.
-// User must be able to save tasks for later viewing.
-// User must be able to mark tasks as completed.
-// User must be able to delete tasks from list.
-// User must be able to customize task list view.
-// App must be intuitive and easy to use.
-
-let listInput = document.querySelector("#listsInput");
-let addListBtn = document.querySelector("#add-list-btn");
-let listGroup1 = document.querySelector("#list-group1");
-
-//TODO: make sure to add an ID generator(?)
-
-let lists = {
-  1: {
-    id: Math.floor(Math.random() * 100),
+let lists = [
+  {
+    id: Math.floor(Math.random() * 10000),
     name: "School",
     todos: [
       {
@@ -34,8 +17,8 @@ let lists = {
       },
     ],
   },
-  2: {
-    id: Math.floor(Math.random() * 100),
+  {
+    id: Math.floor(Math.random() * 10000),
     name: "Home",
     todos: [
       {
@@ -52,4 +35,111 @@ let lists = {
       },
     ],
   },
-};
+  {
+    id: Math.floor(Math.random() * 10000),
+    name: "2024",
+    todos: [
+      {
+        text: "Go to Germany + Spain",
+        completed: false,
+      },
+      {
+        text: "Finish MTECH",
+        completed: false,
+      },
+      {
+        text: "Olivia Rodrigo concert",
+        completed: false,
+      },
+    ],
+  },
+];
+
+//displaying the lists we have
+let groupOfLists = document.querySelector("#lists");
+
+lists.forEach((listItem) => {
+  //indivitual list item
+  let singleListItem = document.createElement("li");
+
+  singleListItem.innerHTML = `<div class= "list-group-item">
+  <li> ${listItem.name}</li> 
+  <i class="fa-solid fa-trash-can"></i>
+  </div>`;
+
+  groupOfLists.appendChild(singleListItem);
+});
+
+//ADD LIST ITEM
+let listsInput = document.querySelector("#listsInput");
+let addListBtn = document.querySelector("#add-list-btn");
+
+//MAKE BUTTON WORK
+addListBtn.addEventListener("click", addList);
+
+//WHAT DOES THE BUTTON DO?
+function addList() {
+  let listNameToAdd = document.createElement("li");
+
+  listNameToAdd.innerHTML = `<div class= "list-group-item">
+  <li> ${listsInput.value}</li> 
+  <i class="fa-solid fa-trash-can"></i>
+  </div>`;
+
+  groupOfLists.appendChild(listNameToAdd);
+}
+
+
+
+
+function renderContent() {
+  // this will hold the html that will be displayed in the sidebar
+  let listsHtml = '<ul class="list-group">';
+
+  // iterate through the lists to get their names
+  lists.forEach((list) => {
+    listsHtml += `<li class="list-group-item">${list.name}</li>`;
+  });
+
+  listsHtml += "</ul>";
+
+  // print out the lists
+  document.getElementById("lists").innerHTML = listsHtml;
+
+  // print out the name of the current list
+  document.getElementById("current-list-name").innerText = currentList.name;
+
+  // iterate over the todos in the current list
+  let todosHtml = '<ul class="list-group-flush">';
+  currentList.todos.forEach((list) => {
+    todosHtml += `<li class="list-group-item">${todo.text}</li>`;
+  });
+
+  // print out the todos
+  document.getElementById("current-list-todos").innerHTML = todosHtml;
+}
+
+// //ADD TODO
+// function addTodo() {
+//   // get the todo text from the todo input box
+//   const text = document.getElementById('taskInput').value;
+//   if(text) {
+//     currentList.todos.push({
+//       text: text,
+//       completed: false
+//     })
+//     render();
+//   }
+//   console.log('todo added!')
+//  }
+
+// User must be able to create multiple lists of tasks.
+// User must be able to edit, delete, and mark tasks.
+// User must be able to clear tasks when they are complete.
+// User must be able to view all tasks in a list.
+// User must be able to search for specific tasks in the list.
+// User must be able to save tasks for later viewing.
+// User must be able to mark tasks as completed.
+// User must be able to delete tasks from list.
+// User must be able to customize task list view.
+// App must be intuitive and easy to use.
