c8cbd7e0960d415ca02c46eec9cfee16c7079526
Nicole Linnarz <mikaelafs17@gmail.com>
2024-03-04 10:30:47 -0700

    identified each section

--- WPDP 1010/golf-score-app/main.js
+++ WPDP 1010/golf-score-app/main.js
@@ -1,4 +1,4 @@
-function getAvailableGolfCourses() {
+async function getAvailableGolfCourses() {
   return fetch(
     "https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/courses.json",
     { mode: "no-cors" }
@@ -7,7 +7,7 @@ function getAvailableGolfCourses() {
   });
 }
 
-function getGolfCourseDetails(golfCourseId) {
+async function getGolfCourseDetails(golfCourseId) {
   return fetch(
     `https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/course${golfCourseId}.json`,
     { mode: "no-cors" }
