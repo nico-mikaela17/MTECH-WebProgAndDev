2b4bcbe5ba299e1f92c5fa1ab3aae9efd18e0308
Nicole Linnarz <mikaelafs17@gmail.com>
2024-03-07 21:27:39 -0700

    Worked with Al - table done

--- WPDP 1010/golf-score-app/main.js
+++ WPDP 1010/golf-score-app/main.js
@@ -1,3 +1,38 @@
+/************************* THE ORDER WE BE WORKING IN *****************************/
+
+/*
+  async function getAvailableGolfCourses() {
+    fetch(url)
+      .then(res => res.json())
+      .then(courses => handleSelect(courses))
+  }
+
+  function handleSelect(courses) {
+    // access first dropdown with document.querySelector
+    // populate course select
+    // add event listener
+    let dropdown1 = document.getElementById("course-select")
+    dropdown1.addEventListener("change", getGolfCourseDetails(dropdown1.value));
+  }
+
+  async function getGolfCourseDetails(id) {
+    fetch(url w/ id)
+      .then(res => res.json())
+      .then(courses => handleSecondSelect(courses))
+  }
+
+  function handleSecondSelect(courses) {
+    // unhides second dropdown
+    // populate second dropdown
+    // adds event listener on change
+    // on change call populateTable(dropdown2.value)
+  }
+
+  function populateTable(details) {
+    // use details data to populate golf table
+  }
+*/
+
 async function getAvailableGolfCourses() {
   try {
     let response = await fetch(
@@ -9,22 +44,43 @@ async function getAvailableGolfCourses() {
     }
 
     let courses = await response.json();
-
-    let courseOptionsHtml = "";
-    courseOptionsHtml += `<option value="" selected>Choose Course</option>`;
-    courses.forEach((course) => {
-      courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`;
-    });
-
-    document.getElementById("course-select").innerHTML = courseOptionsHtml;
+    handleCourseSelect(courses);
   } catch (error) {
-    console.error("Error fetching data:", error);
+    console.error("Error fetching course list:", error);
   }
 }
 
-let teeBoxSelect = document.querySelector("#tee-box-select");
+getAvailableGolfCourses();
 
-//FIXME:
+/************************* THE ORIGINAL CODE IN THE EVENT HANDLER FUNCTION CALL *****************************/
+// async function () {
+//   teeBoxSelect.classList.remove("hidden");
+
+//   // Get the selected course ID
+//   let selectedCourseId = this.value;
+//   // console.log(selectedCourseId);
+
+//   // Call the function to get details for the selected course
+//   await getGolfCourseDetails(selectedCourseId);
+/***********************************************************************************************************/
+
+function handleCourseSelect(courses) {
+  let courseSelect = document.getElementById("course-select");
+
+  let courseOptionsHtml = "";
+  courseOptionsHtml += `<option value="" selected>Choose Course</option>`;
+  courses.forEach((course) => {
+    courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`;
+  });
+  courseSelect.innerHTML = courseOptionsHtml;
+
+  // Event listener for the course selection change
+  courseSelect.addEventListener("change", () => {
+    console.log("Course ID: ", courseSelect.value);
+    getGolfCourseDetails(courseSelect.value);
+  });
+}
+
 async function getGolfCourseDetails(golfCourseId) {
   try {
     let response = await fetch(
@@ -36,82 +92,107 @@ async function getGolfCourseDetails(golfCourseId) {
     }
 
     let courses = await response.json();
-
-    let teeBoxSelectHtml = "";
-    teeBoxSelectHtml += `<option value="" selected>Choose Difficulty</option>`;
-
-    let seenTeeBoxes = new Set();
-
-    //dropdown
-    courses.holes[0].teeBoxes.forEach((teeBox, index) => {
-      teeBoxSelectHtml += seenTeeBoxes.has(teeBox)
-        ? ""
-        : `<option value="${index}">${teeBox.teeType.toUpperCase()}</option>`;
-
-      seenTeeBoxes.add(teeBox);
-    });
-
-    teeBoxSelect.innerHTML = teeBoxSelectHtml;
-    teeBoxSelect.addEventListener("change", populateTable);
-
-    function populateTable() {
-      let table = document.querySelector(".table");
-      table.innerHTML = "";
-      let holesRow = document.createElement("tr");
-
-      let holeTitle = document.createElement("th");
-      holeTitle.textContent = "Holes";
-
-      holesRow.appendChild(holeTitle);
-
-      //create the 1-9 numbers
-      for (i = 0; i < 10; i++) {
-        let holeNumbers = document.createElement("td");
-        holeNumbers.textContent = i;
-        holesRow.appendChild(holeNumbers);
-      }
-
-      let out = document.createElement("th");
-      out.textContent = "Out";
-      holesRow.appendChild(out);
-
-      // let selectedTee = teeBoxSelect.value;
-      table.appendChild(holesRow);
-
-      for (i = 10; i < 19; i++) {
-        let holeNumbers = document.createElement("td");
-        holeNumbers.textContent = i;
-        holesRow.appendChild(holeNumbers);
-      }
-      let inText = document.createElement("th");
-      inText.textContent = "In";
-      holesRow.appendChild(inText);
-
-      let total = document.createElement("th");
-      total.textContent = "TOTAL";
-      holesRow.appendChild(total);
-    }
-    console.log("course selected: ", courses);
+    handleTeeSelect(courses);
   } catch (error) {
-    // holeBoxSelect.innerHTML = holeBoxSelectHtml;
-    // teeBoxSelect.innerHTML = teeBoxSelectHtml;
-    console.error("Error fetching data:", error);
+    console.error("Error fetching course details:", error);
   }
 }
 
-// Event listener for the course selection change
-document
-  .getElementById("course-select")
-  .addEventListener("change", function () {
-    teeBoxSelect.classList.remove("hidden");
+function handleTeeSelect(courses) {
+  let teeBoxSelect = document.querySelector("#tee-box-select");
 
-    // Get the selected course ID
-    let selectedCourseId = this.value;
-    // console.log(selectedCourseId);
+  let teeBoxSelectHtml = "";
+  teeBoxSelectHtml += `<option value="" selected>Choose Difficulty</option>`;
+  let seenTeeBoxes = new Set();
+  courses.holes[0].teeBoxes.forEach((teeBox, index) => {
+    teeBoxSelectHtml += seenTeeBoxes.has(teeBox)
+      ? ""
+      : `<option value="${index}">${teeBox.teeType.toUpperCase()}</option>`;
 
-    // Call the function to get details for the selected course
-    getGolfCourseDetails(selectedCourseId);
+    seenTeeBoxes.add(teeBox);
   });
 
-// Call the function to get the list of available golf courses
-getAvailableGolfCourses();
+  teeBoxSelect.innerHTML = teeBoxSelectHtml;
+  teeBoxSelect.addEventListener("change", () => {
+    console.log("TeeBox index selected: ", teeBoxSelect.value);
+    populateTable(courses, teeBoxSelect.value);
+  });
+}
+
+function populateTable(courses, teeBoxValue) {
+  let table = document.querySelector(".table");
+  table.classList.remove("hidden");
+  table.innerHTML = "";
+
+  //holes
+  let holesRow = document.createElement("tr");
+  let holeTitle = document.createElement("th");
+  holeTitle.textContent = "Holes";
+  holesRow.appendChild(holeTitle);
+  //create the 1-9 numbers
+  for (i = 1; i < 10; i++) {
+    let holeNumbers = document.createElement("td");
+    holeNumbers.textContent = i;
+    holesRow.appendChild(holeNumbers);
+  }
+  let out = document.createElement("th");
+  out.textContent = "Out";
+  holesRow.appendChild(out);
+  for (i = 10; i < 19; i++) {
+    let holeNumbers = document.createElement("td");
+    holeNumbers.textContent = i;
+    holesRow.appendChild(holeNumbers);
+  }
+  let inText = document.createElement("th");
+  inText.textContent = "In";
+  holesRow.appendChild(inText);
+  let total = document.createElement("th");
+  total.textContent = "TOTAL";
+  holesRow.appendChild(total);
+
+  const teeIndex = Number(teeBoxValue);
+
+  table.appendChild(holesRow);
+  table.appendChild(createRow(courses, "yards", teeIndex));
+  table.appendChild(createRow(courses, "par", teeIndex));
+  table.appendChild(createRow(courses, 'hcp', teeIndex));
+}
+
+function createRow(courses, rowName, teeIndex) {
+  let dataRow = document.createElement("tr");
+  let dataTitle = document.createElement("th");
+  dataTitle.textContent = rowName.toUpperCase();
+  dataRow.appendChild(dataTitle);
+  let outSum = 0;
+  let inSum = 0;
+
+  // OUT LOOP
+  for (let i = 0; i < 9; i++) {
+    let dataCellOut = document.createElement("td");
+    dataCellOut.textContent = courses.holes[i].teeBoxes[teeIndex][rowName];
+    outSum += Number(courses.holes[i].teeBoxes[teeIndex][rowName]);
+    dataRow.appendChild(dataCellOut);
+  }
+
+  let outSumCell = document.createElement("th");
+  outSumCell.textContent = outSum;
+  dataRow.appendChild(outSumCell);
+
+  // IN LOOP
+  for (let i = 9; i < 18; i++) {
+    let dataCellIn = document.createElement("td");
+    dataCellIn.textContent = courses.holes[i].teeBoxes[teeIndex][rowName];
+    inSum += Number(courses.holes[i].teeBoxes[teeIndex][rowName]);
+    dataRow.appendChild(dataCellIn);
+  }
+
+  let inSumCell = document.createElement("th");
+  inSumCell.textContent = inSum;
+  dataRow.appendChild(inSumCell);
+
+  let totalCell = document.createElement("th");
+  totalCell.textContent = outSum + inSum;
+  dataRow.appendChild(totalCell);
+
+  return dataRow;
+}
