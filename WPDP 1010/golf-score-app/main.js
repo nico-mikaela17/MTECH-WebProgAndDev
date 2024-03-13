014cedc0f5db2b6325bce1af2c65d24014c8c511
Nicole Linnarz <mikaelafs17@gmail.com>
2024-03-13 09:13:43 -0600

    editing scores works for in/out/total

--- WPDP 1010/golf-score-app/main.js
+++ WPDP 1010/golf-score-app/main.js
@@ -205,36 +205,34 @@ function populateTable(courses, teeBoxValue) {
     let out = document.createElement("th");
     out.textContent = 0;
 
-    let editCommentDisplay = document.createElement("div");
-    editCommentDisplay.classList.add("hidden", "editCommentDisplay");
+    let outeditCommentDisplay = document.createElement("div");
+    outeditCommentDisplay.classList.add("hidden", "editCommentDisplay");
 
-    let holeNumInput = document.createElement("input");
-    holeNumInput.classList.add("hidden", "holeNumInput");
-    holeNumInput.type = "Number";
-    holeNumInput.placeholder = "Score";
-    holeNumInput.textContent = name.text;
+    let outHoleNumInput = document.createElement("input");
+    outHoleNumInput.classList.add("hidden", "holeNumInput");
+    outHoleNumInput.type = "Number";
+    outHoleNumInput.placeholder = "Score";
+    outHoleNumInput.textContent = name.text;
 
-    let saveBtn = document.createElement("button");
-    saveBtn.textContent = "Save";
-    saveBtn.classList.add("hidden", "saveBtn");
+    let outSaveBtn = document.createElement("button");
+    outSaveBtn.textContent = "Save";
+    outSaveBtn.classList.add("hidden", "saveBtn");
 
-    editCommentDisplay.appendChild(holeNumInput);
-    editCommentDisplay.appendChild(saveBtn);
-    out.appendChild(editCommentDisplay);
+    outeditCommentDisplay.appendChild(outHoleNumInput);
+    outeditCommentDisplay.appendChild(outSaveBtn);
+    out.appendChild(outeditCommentDisplay);
 
     out.addEventListener("click", () => {
       console.log("clicked");
 
-      editCommentDisplay.classList.remove("hidden");
-      holeNumInput.classList.remove("hidden");
-      saveBtn.classList.remove("hidden");
+      outeditCommentDisplay.classList.remove("hidden");
+      outHoleNumInput.classList.remove("hidden");
+      outSaveBtn.classList.remove("hidden");
     });
-    saveBtn.addEventListener("click", () => {
-      out.textContent = holeNumInput.value;
-      holeNumInput.classList.add("hidden");
-      saveBtn.classList.add("hidden");
-      // scores.push = holeNumInput.value;
-      // saveToLocalStorage();
+    outSaveBtn.addEventListener("click", () => {
+      out.textContent = outHoleNumInput.value;
+      outHoleNumInput.classList.add("hidden");
+      outSaveBtn.classList.add("hidden");
     });
     playerRow.appendChild(out);
 
@@ -281,46 +279,68 @@ function populateTable(courses, teeBoxValue) {
     let inScore = document.createElement("th");
     inScore.textContent = 0;
 
-    inScore.appendChild(editCommentDisplay);
+    let inEditCommentDisplay = document.createElement("div");
+    inEditCommentDisplay.classList.add("hidden", "editCommentDisplay");
+
+    let inHoleNumInput = document.createElement("input");
+    inHoleNumInput.classList.add("hidden", "holeNumInput");
+    inHoleNumInput.type = "Number";
+    inHoleNumInput.placeholder = "Score";
+    inHoleNumInput.textContent = name.text;
+
+    let inSaveBtn = document.createElement("button");
+    inSaveBtn.textContent = "Save";
+    inSaveBtn.classList.add("hidden", "saveBtn");
 
     inScore.addEventListener("click", () => {
       console.log("clicked");
 
-      editCommentDisplay.classList.remove("hidden");
-      holeNumInput.classList.remove("hidden");
-      saveBtn.classList.remove("hidden");
+      inEditCommentDisplay.classList.remove("hidden");
+      inHoleNumInput.classList.remove("hidden");
+      inSaveBtn.classList.remove("hidden");
     });
-    saveBtn.addEventListener("click", () => {
-      inScore.textContent = holeNumInput.value;
-      holeNumInput.classList.add("hidden");
-      saveBtn.classList.add("hidden");
-      // scores.push = holeNumInput.value;
-      // saveToLocalStorage();
+    inSaveBtn.addEventListener("click", () => {
+      inScore.textContent = inHoleNumInput.value;
+      inHoleNumInput.classList.add("hidden");
+      inSaveBtn.classList.add("hidden");
     });
+    inEditCommentDisplay.appendChild(inHoleNumInput);
+    inEditCommentDisplay.appendChild(inSaveBtn);
+    inScore.appendChild(inEditCommentDisplay);
     playerRow.appendChild(inScore);
 
-
-    //FIXME: editCommentDisplay div only shows on total, even when I click on OUT or IN
     //total
     let total = document.createElement("th");
     total.textContent = 0;
 
-    total.appendChild(editCommentDisplay);
+    let totalEditCommentDisplay = document.createElement("div");
+    totalEditCommentDisplay.classList.add("hidden", "editCommentDisplay");
+
+    let totalHoleNumInput = document.createElement("input");
+    totalHoleNumInput.classList.add("hidden", "holeNumInput");
+    totalHoleNumInput.type = "Number";
+    totalHoleNumInput.placeholder = "Score";
+    totalHoleNumInput.textContent = name.text;
+
+    let totalSaveBtn = document.createElement("button");
+    totalSaveBtn.textContent = "Save";
+    totalSaveBtn.classList.add("hidden", "saveBtn");
 
     total.addEventListener("click", () => {
       console.log("clicked");
 
-      editCommentDisplay.classList.remove("hidden");
-      holeNumInput.classList.remove("hidden");
-      saveBtn.classList.remove("hidden");
+      totalEditCommentDisplay.classList.remove("hidden");
+      totalHoleNumInput.classList.remove("hidden");
+      totalSaveBtn.classList.remove("hidden");
     });
-    saveBtn.addEventListener("click", () => {
-      total.textContent = holeNumInput.value;
-      holeNumInput.classList.add("hidden");
-      saveBtn.classList.add("hidden");
-      // scores.push = holeNumInput.value;
-      // saveToLocalStorage();
+    totalSaveBtn.addEventListener("click", () => {
+      total.textContent = totalHoleNumInput.value;
+      totalHoleNumInput.classList.add("hidden");
+      totalSaveBtn.classList.add("hidden");
     });
+    totalEditCommentDisplay.appendChild(totalHoleNumInput);
+    totalEditCommentDisplay.appendChild(totalSaveBtn);
+    total.appendChild(totalEditCommentDisplay);
     playerRow.appendChild(total);
 
     table.appendChild(playerRow);
