c1e3d2d54cd5696a4a1cc65dc815e62af4ef7dd1
Nicole Linnarz <mikaelafs17@gmail.com>
2024-05-02 10:33:51 -0600

    able to read data in other folders based on certain conditions

---
+++ TEWP 1040/in-class/libExercise.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+//what's in the lib folder?
+fs.readdir("./lib", (err, data) => {
+  if (err) throw err;
+  console.log(`Contents of lib folder: ${data}`);
+  data.forEach((element) =>
+    fs.stat(`./lib/${element}`, (err, data) => {
+      if (err) throw err;
+      if (data.isFile()) {
+        fs.readFile(`./lib/${element}`, "utf-8", (err, moreData) => {
+          if (err) throw err;
+          console.log(moreData);
+        });
+      }
+    })
+  );
+});
+
+//array, loop over that, call fs.stats in each, callback - isFile(), if true - readFile
