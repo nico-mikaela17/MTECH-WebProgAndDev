e787b1ccf2746ee682c7f034680062d5cd0b7af2
Nicole Linnarz <mikaelafs17@gmail.com>
2024-05-02 09:34:51 -0600

    exact match and depending on user input

--- TEWP 1040/in-class/exercise5-2.js
+++ TEWP 1040/in-class/exercise5-2.js
@@ -1,16 +1,20 @@
 const fs = require("fs");
 
+const fileToRead = process.argv[2];
+const fileToWrite = process.argv[3];
+
 //get data
-fs.readFile("./bacon.txt", "utf8", (err, data) => {
+fs.readFile(fileToRead, "utf8", (err, data) => {
   if (err) throw err;
-  console.log(data);
-  const wordToFind = /bacon/gi;
-  const foundWords = data.match(wordToFind);
+  console.log(`Finding data...`);
+  //search globaly (multiple words, not just the first one) AND exact match
+  const wordToFind = /\bbacon\b/gi;
+  const foundWords = data.match(wordToFind).length;
   //count how many "bacon"
-  console.log(foundWords.length);
+  console.log(`Bacon was found ${foundWords} times`);
   //replace word bacon for tasty
   const tastyText = data.replace(wordToFind, "tasty");
-  fs.writeFile("tasty.txt", tastyText, (err) => {
+  fs.writeFile(fileToWrite, tastyText, (err) => {
     if (err) throw err;
   });
   console.log();
